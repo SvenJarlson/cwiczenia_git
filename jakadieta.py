1c7a2b925a9fc74c3829fefaabea268249c1d96c
Daniel_wsb <Daniel@wsb.pl>
2022-03-26 11:36:37 +0100

    Poprawki_w_diecie

---
+++ jakadieta.py
@@ -0,0 +1,10 @@
+def jakadieta :
+    dieta=str(input('Jak się odżywiasz? Zdrowo , Normarlnie , czy Źle'))
+    if dieta == 'Zdrowo':
+        pkt=pkt*0.5
+    elif dieta == 'Normalnie':
+        pkt=pkt
+    elif dieta == 'Źle':
+        pkt=pkt*2
+    else :
+        print('Jak niby się odżywiasz? Porsze wpisać tak jak w pytaniu'
