9dbd328d4f42ae3cc26b93d131cd38525ee83adf
ewelina- <ewelina@gmail.com>
2022-03-26 12:26:40 +0100

    działa

--- zdrowie_funkcje.py
+++ zdrowie_funkcje.py
@@ -1,19 +1,9 @@
-#from jakadieta.py import jakadieta
-from stres import stres
-rodzaj_pracy = int(input("Jaką pracę wykonujesz? Podaj odpowiednią liczbę: \n Praca umysłowa:1 \n Praca fizyczna na wysokości:2 \n Praca fizyczna pod ziemią:3 \n Praca fizyczna inna:4 \n Praca z dziećmi:5 \n Praca z chorymi:6 \n Inne:7"))
-tiki = int(input("Czy posiadasz tiki nerwowe? Wpisz 1 jeśli tak, 0 jeśli nie"))
-pkt=0
-stres(rodzaj_pracy, tiki)
-print(pkt)
+from cwiczenia_git.stres import *
 
+rodzaj_pracy = int(input("Jaką pracę wykonujesz? Podaj odpowiednią liczbę: \n Praca umysłowa:1 \n Praca fizyczna na wysokości:2 \n Praca fizyczna pod ziemią:3 \n Praca fizyczna inna:4 \n Praca z dziećmi:5 \n Praca z chorymi:6 \n Inne:7 \n"))
+tiki = int(input("Czy posiadasz tiki nerwowe? Wpisz 1 jeśli tak, 0 jeśli nie \n"))
 
-#def poziomstresu(rodzaj_pracy, tiki_nerwowe):
+s = stres(rodzaj_pracy, tiki)
+print(s)
 
-
-
-#def ileruchu(ile_zwyklego_ruchu, ile_cwiczen):
-
-
-
-#jakadieta()
 
